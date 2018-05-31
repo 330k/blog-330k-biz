865a8745865f2d4f4e35809d056ec7ca70112714
330k <webmaster@330k.biz>
2018-05-31 14:10:31 -0400

    test

--- wp-login.php
+++ wp-login.php
@@ -14,7 +14,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>ログイン &lsaquo; 金融エンジニアの屁理屈ブログ &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://blog.330k.biz/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.6' type='text/css' media='all' />
+<link rel='stylesheet' href='https://330k.biz/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.6' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://ja.wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="https://blog.330k.biz/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://330k.biz/wp-login.php" method="post">
 	<p>
 		<label for="user_login">ユーザー名またはメールアドレス<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -34,13 +34,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> ログイン状態を保存する</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="ログイン" />
-		<input type="hidden" name="redirect_to" value="https://blog.330k.biz/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://330k.biz/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="https://blog.330k.biz/wp-login.php?action=lostpassword">パスワードをお忘れですか ?</a>
+	<a href="https://330k.biz/wp-login.php?action=lostpassword">パスワードをお忘れですか ?</a>
 </p>
 
 <script type="text/javascript">
@@ -57,7 +57,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://blog.330k.biz/">&larr; 金融エンジニアの屁理屈ブログ に戻る</a></p>
+	<p id="backtoblog"><a href="https://330k.biz/">&larr; 金融エンジニアの屁理屈ブログ に戻る</a></p>
 		
 	</div>
 
